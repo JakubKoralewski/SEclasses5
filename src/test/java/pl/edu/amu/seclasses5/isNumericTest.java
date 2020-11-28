d582a86787cd7118bbc7c0c9580c8cdb4d640c1e
Jakub Koralewski <jakub.koralewski1@gmail.com>
2020-11-28 21:39:29 +0100

    sneaky trap bomb has been defused

--- src/test/java/pl/edu/amu/seclasses5/isNumericTest.java
+++ src/test/java/pl/edu/amu/seclasses5/isNumericTest.java
@@ -24,8 +24,6 @@ public class isNumericTest {
         boolean expResult = false;
         boolean result = isNumeric.isNumeric(str);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
